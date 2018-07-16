41dcf34a25c86df2670318054621fb16e5bb7124
Florent <dardenneflorent@outlook.com>
2018-07-16 03:36:56 +0200

    Button add workhour, list all the workhour in hours.html

--- userboard/views.py
+++ userboard/views.py
@@ -3,6 +3,7 @@ from .models import SignUpForm
 from .models import Work, WorkHour
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
+import datetime
 # Create your views here.
 
 @login_required
@@ -35,6 +36,7 @@ def profile(request):
 
 @login_required
 def work(request):
+    #TODO Verify the form
     work_list = Work.objects.filter(user=request.user)
     flag = ''
     if request.method == "POST":
@@ -97,6 +99,44 @@ def signup(request):
 
 @login_required
 def hour(request):
-    return render(request, 'userboard/hours.html')
+    #TODO VERIFY THE FORM
+
+    user = request.user
+    work = Work.objects.get(user=user)
+
+    if request.method == "POST":
+
+        begin_date_hour_list = request.POST.get("beginning").split("T")
+        begin_date_list = begin_date_hour_list[0].split('-')
+        begin_hour_list = begin_date_hour_list[1].split(':')
+
+        end_date_hour_list = request.POST.get("end").split("T")
+        end_date_list = end_date_hour_list[0].split('-')
+        end_hour_list = end_date_hour_list[1].split(':')
+
+        begin_date = datetime.datetime(day=int(begin_date_list[2]), month=int(begin_date_list[1]),
+                                       year=int(begin_date_list[0]), minute=int(begin_hour_list[1]),
+                                       hour=int(begin_hour_list[0]))
+
+        end_date = datetime.datetime(day=int(end_date_list[2]), month=int(end_date_list[1]),
+                                       year=int(end_date_list[0]), minute=int(end_hour_list[1]),
+                                       hour=int(end_hour_list[0]))
+
+        new_hour = WorkHour(work=work, beginning_date=begin_date, end_date=end_date)
+        new_hour.save()
+
+    workhours_list = WorkHour.objects.filter(work=work)
+    context_list = []
+    for hours in workhours_list:
+        context_list.append({
+            'beginning_date':hours.beginning_date.strftime("%d/%m/%y %H:%M"),
+            'end_date':hours.end_date.strftime("%d/%m/%y %H:%M")
+        })
+
+    context = {'workhours_list': context_list}
+
+
+
+    return render(request, 'userboard/hours.html', context)
 
 
