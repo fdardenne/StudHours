6d6c24d44622835d9cc1baeb036941e3c3bd37c4
Florent <dardenneflorent@outlook.com>
2018-07-18 03:37:25 +0200

    added salary to workhours models, remove extra_hour from work

--- userboard/models.py
+++ userboard/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+import datetime
 
 
 class Work(models.Model):
@@ -11,12 +12,15 @@ class Work(models.Model):
     tax_percent = models.DecimalField(decimal_places=2, max_digits=5)
     extra_public_holiday_percent= models.DecimalField(decimal_places=2, max_digits=5)
     extra_per_day = models.DecimalField(decimal_places=2, max_digits=5)
-    extra_additional_hour_percent = models.DecimalField(decimal_places=2, max_digits=5)
 
 class WorkHour(models.Model):
     work = models.ForeignKey(Work, on_delete=models.CASCADE)
     beginning_date = models.DateTimeField()
     end_date = models.DateTimeField()
+    pause_duration = models.DurationField(default=datetime.timedelta(days=0))
+    public_holiday = models.BooleanField(default=False)
+    salary_earned = models.DecimalField(decimal_places=2, max_digits=6, default=0)
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
--- userboard/views.py
+++ userboard/views.py
@@ -10,35 +10,20 @@ import calendar
 @login_required
 def userboard(request):
 
+    # init variables
     day_max = calendar.monthrange(datetime.datetime.now().date().year, datetime.datetime.now().date().month)[1]
+    label = []
+    color = []
+    border_color = []
+    first_day_week = calendar.monthrange(datetime.datetime.now().date().year, datetime.datetime.now().date().month)[0]
 
-    user = request.user
-    work = Work.objects.get(user=user)
-    workhour_month_list = WorkHour.objects.filter(work=work, beginning_date__month=datetime.datetime.now().date().month)
-
-    moneys = [0] * day_max
-
-    total_money = 0
-    total_hours = 0
-
-    for workhour in workhour_month_list:
-        day = workhour.beginning_date.date().day
-        time_worked = float((workhour.end_date - workhour.beginning_date).seconds) / 3600
-        money_earned = time_worked * float(work.salary)
-
-        total_hours += time_worked
-        total_money += round(time_worked * float(work.salary),2)
-        moneys[day-1] += round(money_earned, 2)
-
-
-
-    color_template = ["rgba(255, 99, 132, 0.2)",
-                      "rgba(54, 162, 235, 0.2)",
-                      "rgba(255, 206, 86, 0.2)",
-                      "rgba(75, 192, 192, 0.2)",
-                      "rgba(153, 102, 255, 0.2)",
-                      "rgba(255, 159, 64, 0.2)",
-                      "rgba(244, 66, 244,0.2)"]
+    color_template = ['rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(244, 66, 244,0.2)']
 
     border_color_template = ['rgba(255,99,132,1)',
                              'rgba(54, 162, 235, 1)',
@@ -49,31 +34,53 @@ def userboard(request):
                              'rgba(244, 66, 244, 1)',
                              ]
 
-    label = []
-    color = []
-    border_color = []
-
-    first_day_week = calendar.monthrange(datetime.datetime.now().date().year, datetime.datetime.now().date().month)[0]
-
-    #BUILD CONTEXT
+    # build chart label / color / border color
     for x in range(day_max):
-        label.append(x+1)
+        label.append(x + 1)
 
         first_day_week = 0 if first_day_week == 7 else first_day_week
         color.append(color_template[first_day_week])
         border_color.append(border_color_template[first_day_week])
         first_day_week += 1
 
-    context = {
-        'day': label,
-        'money': moneys,
-        'color': color,
-        'border_color': border_color,
-        'total_money': total_money,
-        'total_hours': total_hours
-    }
 
-    return render(request, 'userboard/dashboard.html', context)
+
+    # get database information
+
+    try:
+        user = request.user
+        work = Work.objects.get(user=user)
+        workhour_month_list = WorkHour.objects.filter(work=work, beginning_date__month=datetime.datetime.now().date().month)
+
+        moneys = [0] * day_max
+
+        total_money = 0
+        total_hours = 0
+
+        for workhour in workhour_month_list:
+            day = workhour.beginning_date.date().day
+            time_worked = float((workhour.end_date - workhour.beginning_date).seconds) / 3600
+            money_earned = float(round(workhour.salary_earned, 2))
+
+            total_hours += time_worked
+            total_money += money_earned
+            moneys[day-1] += money_earned
+
+        context = {
+            'day': label,
+            'money': moneys,
+            'color': color,
+            'border_color': border_color,
+            'total_money': total_money,
+            'total_hours': total_hours
+        }
+
+        return render(request, 'userboard/dashboard.html', context)
+
+    except Exception as e:
+        print(e)
+        return render(request, 'userboard/dashboard.html', {'day': label, 'money': [], 'color':color, 'border_color': border_color, 'total_money': 0, 'total_hours': 0})
+
 
 @login_required
 def profile(request):
@@ -96,26 +103,24 @@ def work(request):
     #TODO Verify the form
     work_list = Work.objects.filter(user=request.user)
     flag = ''
-    if request.method == "POST":
+    if request.method == 'POST':
         if len(work_list) == 0:
 
             new_work = Work(user=request.user, name=request.POST.get('name'),
                         salary=request.POST.get('price'),
                         tax_percent=request.POST.get('tax'),
                         extra_public_holiday_percent=request.POST.get('holiday'),
-                        extra_per_day=request.POST.get('extra'),
-                        extra_additional_hour_percent = request.POST.get('extra_add'))
+                        extra_per_day=request.POST.get('extra'),)
             new_work.save()
             flag = 'created'
 
         else:
             work_modified = Work.objects.get(user=request.user)
-            work_modified.name =request.POST.get('name')
+            work_modified.name = request.POST.get('name')
             work_modified.salary = request.POST.get('price')
-            work_modified.tax_percent =  request.POST.get('tax')
+            work_modified.tax_percent = request.POST.get('tax')
             work_modified.extra_public_holiday_percent = request.POST.get('holiday')
             work_modified.extra_per_day = request.POST.get('extra')
-            work_modified.extra_additional_hour_percent = request.POST.get('extra_add')
             work_modified.save()
             flag = 'modified'
 
@@ -125,11 +130,11 @@ def work(request):
                     'salary': work.salary,
                     'tax': work.tax_percent,
                     'extra_public_holiday': work.extra_public_holiday_percent,
-                    'extra_additional_hour': work.extra_additional_hour_percent,
                     'extra_day': work.extra_per_day,
                     'flag': flag,
                     }
-    except:
+    except Exception as e:
+        print(e)
         context = {}
     return render(request, 'userboard/work.html', context)
 
@@ -139,7 +144,7 @@ def homepage(request):
 
 
 def signup(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
@@ -159,15 +164,24 @@ def hour(request):
     #TODO VERIFY THE FORM
 
     user = request.user
-    work = Work.objects.get(user=user)
+    try:
+        work = Work.objects.get(user=user)
+    except:
+        return render(request, 'userboard/hours.html', {'flag': 'error'})
 
-    if request.method == "POST":
+    if request.method == 'POST':
 
-        begin_date_hour_list = request.POST.get("beginning").split("T")
+        is_public_holiday = request.POST.get('public_holiday')
+        pause = request.POST.get('pause')
+        duration_pause = datetime.timedelta(minutes=int(pause))
+        begin_date_hour_list = request.POST.get('beginning').split('T')
         begin_date_list = begin_date_hour_list[0].split('-')
         begin_hour_list = begin_date_hour_list[1].split(':')
 
-        end_date_hour_list = request.POST.get("end").split("T")
+        print(is_public_holiday)
+
+
+        end_date_hour_list = request.POST.get('end').split('T')
         end_date_list = end_date_hour_list[0].split('-')
         end_hour_list = end_date_hour_list[1].split(':')
 
@@ -176,25 +190,29 @@ def hour(request):
                                        hour=int(begin_hour_list[0]))
 
         end_date = datetime.datetime(day=int(end_date_list[2]), month=int(end_date_list[1]),
-                                       year=int(end_date_list[0]), minute=int(end_hour_list[1]),
-                                       hour=int(end_hour_list[0]))
+                                     year=int(end_date_list[0]), minute=int(end_hour_list[1]),
+                                     hour=int(end_hour_list[0]))
 
-        new_hour = WorkHour(work=work, beginning_date=begin_date, end_date=end_date)
+        salary_earned = round((float((end_date-begin_date).seconds)/3600) * float(work.salary),2)
+
+        new_hour = WorkHour(work=work, beginning_date=begin_date, end_date=end_date, salary_earned= salary_earned,
+                            public_holiday=is_public_holiday,
+                            pause_duration=duration_pause)
         new_hour.save()
 
     workhours_list = WorkHour.objects.filter(work=work)
     context_list = []
+
     for hours in workhours_list:
         context_list.append({
-            'beginning_date':hours.beginning_date.strftime("%d/%m/%y %H:%M"),
-            'end_date':hours.end_date.strftime("%d/%m/%y %H:%M"),
-            'time': float((hours.end_date-hours.beginning_date).seconds)/3600,
+            'beginning_date':hours.beginning_date.strftime('%d/%m/%y %H:%M'),
+            'end_date':hours.end_date.strftime('%d/%m/%y %H:%M'),
+            'time': round(float((hours.end_date-hours.beginning_date).seconds)/3600, 2),
+            'salary': hours.salary_earned,
+            'pause': hours.pause_duration
         })
 
-    context = {'workhours_list': context_list}
 
 
-
-    return render(request, 'userboard/hours.html', context)
-
+    return render(request,'userboard/hours.html',{'workhours_list': context_list})
 
