b2f51dddfc3427023ed47e2009e79c25197a3bb1
Florent <dardenneflorent@outlook.com>
2018-07-24 13:46:46 +0200

    Chart mobile responsive, brand name changed

--- StudentHours/settings.py
+++ StudentHours/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
