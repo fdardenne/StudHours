9459435d8d427ffb71b5904d0794c611fbc0e7a1
Florent <dardenneflorent@outlook.com>
2018-07-28 23:52:26 +0200

    Changed admin url

--- StudentHours/urls.py
+++ StudentHours/urls.py
@@ -25,7 +25,7 @@ handler500 = views.handler500
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('admin/', admin.site.urls),
+    path('adminManagement/', admin.site.urls),
     path('viewboard/', include('userboard.urls')),
     path('accounts/signup/', views.signup, name="signup"),
     path('accounts/', include('django.contrib.auth.urls')),
